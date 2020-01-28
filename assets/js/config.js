2ff31a2bb86258f0e191b443108db4ea186f7616
luokangyuan <243518985@qq.com>
2020-01-28 13:54:12 +0800

    :sparkles: 赞助墙基本完成

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,53 +5,8 @@ let friend = [{
     github: "https://github.com/",
     desc: "不忘初心，努力做有价值的技术文章"
 
-},{
-    name: "李四",
-    image: "http://image.luokangyuan.com/1_qq_27922023.jpg",
-    website: "http://luokangyuan.com/",
-    github: "https://github.com/",
-    desc: "不忘初心，努力做有价值的技术文章"
-
-},{
-    name: "李四",
-    image: "http://image.luokangyuan.com/1_qq_27922023.jpg",
-    website: "http://luokangyuan.com/",
-    github: "https://github.com/",
-    desc: "不忘初心，努力做有价值的技术文章"
-
-},{
-    name: "李四",
-    image: "http://image.luokangyuan.com/1_qq_27922023.jpg",
-    website: "http://luokangyuan.com/",
-    github: "https://github.com/",
-    desc: "不忘初心，努力做有价值的技术文章"
-
-},{
-    name: "李四",
-    image: "http://image.luokangyuan.com/1_qq_27922023.jpg",
-    website: "http://luokangyuan.com/",
-    github: "https://github.com/",
-    desc: "不忘初心，努力做有价值的技术文章"
-
-},{
-    name: "李四",
-    image: "http://image.luokangyuan.com/1_qq_27922023.jpg",
-    website: "http://luokangyuan.com/",
-    github: "https://github.com/",
-    desc: "不忘初心，努力做有价值的技术文章"
-
-},{
-    name: "李四",
-    image: "http://image.luokangyuan.com/1_qq_27922023.jpg",
-    website: "http://luokangyuan.com/",
-    github: "https://github.com/",
-    desc: "不忘初心，努力做有价值的技术文章"
-
-},{
-    name: "李四",
-    image: "http://image.luokangyuan.com/1_qq_27922023.jpg",
-    website: "http://luokangyuan.com/",
-    github: "https://github.com/",
-    desc: "不忘初心，努力做有价值的技术文章"
-
-}]
+}];
+let sponsors = [{
+    name: "张三",
+    money: "5元"
+}];
